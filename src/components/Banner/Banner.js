07e0693e50bcc4ce6bc4cb31e84ffaa0a26dbf23
Lucas Alves de Abreu <52301776+lucas-Alves26@users.noreply.github.com>
2023-07-13 14:01:50 -0300

    Revert "fix(banner)"
    This reverts commit fa0fad9bf87fa9c545a3f59d16dc8ee147e14e02.

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,6 @@
 import './Banner.css'
 
 function Banner(){
-    //teste pull
     return <header className='banner'>
         <img src="/imagens/banner.png" alt="banner principal da página do organograma"/>
         </header>
