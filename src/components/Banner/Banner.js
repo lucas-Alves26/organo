d532f0c3d991706f3ac66563b3376f7ac97005cc
Lucas Alves de Abreu <52301776+lucas-Alves26@users.noreply.github.com>
2023-07-13 13:35:57 -0300

    feat(banner.css)

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+function Banner(){
+    return <header className='banner'>
+        <img src="/imagens/banner.png" alt="banner principal da página do organograma"/>
+        </header>
+    
+}
+
+export default Banner
